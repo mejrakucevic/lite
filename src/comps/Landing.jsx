58c5b81649fd1bf48ea36f6c5a5daa2d637dd3f1
mejrakucevic <mejrakucevic420@gmail.com>
2024-05-04 21:10:05 +0200

    .

--- src/comps/Landing.jsx
+++ src/comps/Landing.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../comps/Landing.css";
-import BmiCalc from "./pages/BmiCalculator";
 
 function Landing() {
   return (
     <body>
       <div className="container">
-        <h1>Welcome to WLEasy</h1>
+        <h1>Welcome to Lite</h1>
         <Link to="/bmicalc">
           <button>Start</button>
         </Link>
